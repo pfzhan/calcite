527575ff307ce7763cea6fbd4e075e068e53fdc0
xinbei <xinbei.fu.gr@dartmouth.edu>
2020-11-16 16:28:32 +0800

    KE-19732 make floor function not reducible

--- core/src/main/java/org/apache/calcite/sql/type/ReturnTypes.java
+++ core/src/main/java/org/apache/calcite/sql/type/ReturnTypes.java
@@ -595,6 +595,29 @@ public abstract class ReturnTypes {
   };
 
   /**
+   * return timestamp for date type
+   */
+  public static final SqlReturnTypeInference FLOOR_DATETIME_TO_TIMESTAMP = opBinding -> {
+    RelDataType type1 = opBinding.getOperandType(0);
+    if (SqlTypeUtil.isDatetime(type1)) {
+      RelDataType ret;
+      ret = opBinding.getTypeFactory().createSqlType(SqlTypeName.TIMESTAMP);
+      if (type1.isNullable()) {
+        ret = opBinding.getTypeFactory().createTypeWithNullability(ret, true);
+      }
+      return ret;
+    }
+    return null;
+  };
+
+
+  /**
+   * Type-inference strategy for floor, ceiling
+   */
+  public static final SqlReturnTypeInference ARG0_OR_EXACT_NO_SCALE_OR_TIMESTAMP =
+      DECIMAL_SCALE0.orElse(NUMERIC_TO_LONG).orElse(FLOOR_DATETIME_TO_TIMESTAMP).orElse(ARG0);
+
+  /**
    * Type-inference strategy whereby the result type of a call is
    * {@link #DECIMAL_SCALE0} with a fallback to {@link #ARG0} This rule
    * is used for floor, ceiling.
