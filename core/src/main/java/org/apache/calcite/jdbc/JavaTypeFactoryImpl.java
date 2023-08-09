d85a097bae774b1a246f1ce37df664d738717057
Pengfei Zhan <dethrive@gmail.com>
2023-08-09 17:03:07 +0800

    KE-42354 & KE-42366 split function support, followed by KE-41878. cherry-pick some code from CALCITE-3429

--- core/src/main/java/org/apache/calcite/jdbc/JavaTypeFactoryImpl.java
+++ core/src/main/java/org/apache/calcite/jdbc/JavaTypeFactoryImpl.java
@@ -34,6 +34,7 @@ import org.apache.calcite.sql.type.IntervalSqlType;
 import org.apache.calcite.sql.type.JavaToSqlTypeConversionRules;
 import org.apache.calcite.sql.type.SqlTypeFactoryImpl;
 import org.apache.calcite.sql.type.SqlTypeName;
+import org.apache.calcite.sql.type.SqlTypeUtil;
 import org.apache.calcite.util.Pair;
 import org.apache.calcite.util.Util;
 
@@ -253,11 +254,30 @@ public class JavaTypeFactoryImpl
           type.getFieldNames());
     }
     if (type instanceof JavaType) {
-      return typeFactory.createTypeWithNullability(
-          typeFactory.createSqlType(type.getSqlTypeName()),
-          type.isNullable());
+      SqlTypeName sqlTypeName = type.getSqlTypeName();
+      final RelDataType relDataType;
+      if (SqlTypeUtil.isArray(type)) {
+        // Transform to sql type, take care for two cases:
+        // 1. type.getJavaClass() is collection with erased generic type
+        // 2. ElementType returned by JavaType is also of JavaType,
+        // and needs conversion using typeFactory
+        final RelDataType elementType =
+                toSqlTypeWithNullToAny(typeFactory, type.getComponentType());
+        relDataType = typeFactory.createArrayType(elementType, -1);
+      } else {
+        relDataType = typeFactory.createSqlType(sqlTypeName);
+      }
+      return typeFactory.createTypeWithNullability(relDataType, type.isNullable());
     }
     return type;
+  }
+
+  private static RelDataType toSqlTypeWithNullToAny(
+          final RelDataTypeFactory typeFactory, RelDataType type) {
+    if (type == null) {
+      return typeFactory.createSqlType(SqlTypeName.ANY);
+    }
+    return toSql(typeFactory, type);
   }
 
   public Type createSyntheticType(List<Type> types) {
