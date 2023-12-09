14cc0644c8fd3f319601004e785eecf57881b067
Pengfei Zhan <dethrive@gmail.com>
2023-12-09 16:33:10 +0800

    KE-git-issue15305 Correct overflow of function avg in AggregateReduceFunctionsRule

--- core/src/main/java/org/apache/calcite/rel/rules/AggregateReduceFunctionsRule.java
+++ core/src/main/java/org/apache/calcite/rel/rules/AggregateReduceFunctionsRule.java
@@ -438,11 +438,12 @@ public class AggregateReduceFunctionsRule
             aggCallMapping,
             oldAggRel.getInput()::fieldIsNullable);
 
-    final RelDataTypeFactory typeFactory = oldAggRel.getCluster().getTypeFactory();
-    final RelDataType avgType =
-        typeFactory.createTypeWithNullability(oldCall.getType(),
-            numeratorRef.getType().isNullable());
-    numeratorRef = rexBuilder.ensureType(avgType, numeratorRef, true);
+    // The cast doesn't seem to be necessary and may cause overflow if it is a downcast.
+    // final RelDataTypeFactory typeFactory = oldAggRel.getCluster().getTypeFactory();
+    // final RelDataType avgType =
+    //     typeFactory.createTypeWithNullability(oldCall.getType(),
+    //         numeratorRef.getType().isNullable());
+    // numeratorRef = rexBuilder.ensureType(avgType, numeratorRef, true);
     final RexNode divideRef =
         rexBuilder.makeCall(SqlStdOperatorTable.DIVIDE, numeratorRef, denominatorRef);
     return rexBuilder.makeCast(oldCall.getType(), divideRef);
